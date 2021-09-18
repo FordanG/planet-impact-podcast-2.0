424f583b35498296c421f734a8d413cd447f8332
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-18 11:36:17 +0800

    Update Episodes

--- pages/episodes/_slug.vue
+++ pages/episodes/_slug.vue
@@ -1,45 +1,35 @@
 <template>
-  <section class="px-5 md:px-0 space-y-8">
-    {{ episode.url }}
+  <section class="container pt-20 md:px-0 space-y-10">
     <iframe
-      :src="episode.link"
+      :src="episode.url"
       height="100%"
       width="100%"
       frameborder="0"
       scrolling="no"
     ></iframe>
-    <div class="title space-y-2 mx-auto xl:mt-32 lg:mt-24 md:mt-16 mt-12">
-      <h1
-        class="text-2xl text-center md:text-left md:text-5xl font-semibold text-primary"
-      >
-        {{ episode.title }}
-      </h1>
-      <hr class="border-2 border-secondary" />
 
-      <div class="flex space-x-4 justify-center md:justify-start">
-        <div class="text-center md:text-left">
-          <p class="block underline">{{ episode.name }}</p>
-        </div>
+    <div class="flex">
+      <div class="w-1/2 px-5">
+        <h1 class="text-3xl font-bold text-secondary">{{ episode.name }}</h1>
+        <hr />
+        <nuxt-content :document="episode" class="prose prose-xl mx-auto" />
+      </div>
+      <div class="w-1/2 px-5">
+        <img :src="episode.image" :alt="episode.title" />
+
+        <p>{{ episode.contact }}</p>
+        <nuxt-content :document="episode.contact" class="prose" />
       </div>
     </div>
-
-    <div class="">
-      <img
-        :src="episode.image"
-        :alt="episode.title"
-        class="object-cover w-screen blogImage"
-      />
-    </div>
-    <article class="container">
-      <nuxt-content :document="episode" class="prose prose-xl mx-auto" />
-    </article>
   </section>
 </template>
 
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const episode = await $content('episodes', params.slug).fetch()
+    const episode = await $content('episodes', params.slug)
+      .sortBy('number')
+      .fetch()
 
     return { episode }
   },
