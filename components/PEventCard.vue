dfde4e26f6153b97ef442d7b567820661f98b5d2
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-20 02:11:52 +0800

    Create PEventCard.vue

---
+++ components/PEventCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex lg:flex-row flex-col">
+    <div class="px-5 w-full lg:order-1 lg:w-1/2 flex flex-col mb-4">
+      <img
+        :src="details.image"
+        :alt="details.image"
+        class="block h-full w-full object-cover"
+      />
+    </div>
+    <div
+      class="w-full lg:order-2 lg:w-1/2 flex flex-col lg:space-y-4 space-y-1"
+    >
+      <h2 class="text-center text-2xl font-bold lg:text-3xl text-primary">
+        {{ details.title }}
+      </h2>
+      <p class="text-center text-lg font-semibold lg:text-xl text-secondary">
+        {{ details.date }}
+      </p>
+      <hr class="py-2" />
+      <p class="prose prose-md">{{ details.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    details: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
