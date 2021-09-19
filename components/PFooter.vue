a48b3d38f4bd482e10590f409845893767cc8ec3
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-20 01:57:15 +0800

    Add UN Support in Footer

--- components/PFooter.vue
+++ components/PFooter.vue
@@ -1,7 +1,38 @@
 <template>
-  <footer class="bg-primary w-full">
-    <div
-      class="
+  <div>
+    <section id="un-support" class="px-2 py-4">
+      <div class="container flex flex-col items-center">
+        <div class="flex w-full px-4 md:w-1/3">
+          <div class="flex justify-center items-center w-1/2 ">
+            <img
+              style="border-right: #111 1px solid;"
+              class="pr-5"
+              height="50"
+              src="~assets/img/planet-impact-podcast-circular-logo.png"
+              alt="Planet Impact Podcast Circular Logo"
+            />
+          </div>
+          <div class="flex justify-center items-center w-1/2">
+            <img
+              class="pl-5"
+              height="140"
+              src="~assets/img/E_SDG_logo_without_UN_emblem_Square_Transparent_WEB.png"
+              alt="UN SDG 17"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col text-center">
+            <p>
+              Planet Impact Podcast supports the Sustainable Development Goals
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="bg-primary w-full">
+      <div
+        class="
         container
         py-10
         grid
@@ -11,33 +42,33 @@
         px-5
         xl:px-0
       "
-    >
-      <div class="flex flex-col text-white">
-        <h3>Connect with Us</h3>
-        <div class="space-y-2">
-          <div>
-            <a href="mailto:thegoodpeoplepodcastseries@gmail.com">
-              <email-icon class="pr-2 inline" />
-              thegoodpeoplepodcastseries@gmail.com</a
-            >
-          </div>
-          <div>
-            <a href="tel:+34653064589"
-              ><phone-icon class="pr-2 inline" />+34 653064589</a
-            >
+      >
+        <div class="flex flex-col text-white">
+          <h3>Connect with Us</h3>
+          <div class="space-y-2">
+            <div>
+              <a href="mailto:thegoodpeoplepodcastseries@gmail.com">
+                <email-icon class="pr-2 inline" />
+                thegoodpeoplepodcastseries@gmail.com</a
+              >
+            </div>
+            <div>
+              <a href="tel:+34653064589"
+                ><phone-icon class="pr-2 inline" />+34 653064589</a
+              >
+            </div>
           </div>
         </div>
+        <div class="flex flex-col text-white">
+          <h3>India Address</h3>
+          <p>
+            Ahmednagar, 414001 - India
+          </p>
+        </div>
       </div>
-      <div class="flex flex-col text-white">
-        <h3>India Address</h3>
-        <p>
-          Ahmednagar, 414001 - India
-        </p>
-      </div>
-    </div>
-    <div class="w-full bg-secondary text-primary py-2 ">
-      <div
-        class="
+      <div class="w-full bg-secondary text-primary py-2 ">
+        <div
+          class="
           container
           flex flex-col
           md:flex-row
@@ -46,28 +77,29 @@
           px-5
         xl:px-0
         "
-      >
-        <p id="copyright" class="text-white order-1 md:order-0">
-          All Rights Reserved | Planet Impact Podcast
-          {{ new Date().getFullYear() }}
-        </p>
-        <div class="flex space-x-2 order-0 md:order-1">
-          <a href="https://www.facebook.com/135manthan"
-            ><facebook-icon class="primary"
-          /></a>
-          <a href="https://twitter.com/ManthanShah135"
-            ><twitter-icon class="primary"
-          /></a>
-          <a href="https://www.instagram.com/planetimpactpod/?hl=en"
-            ><instagram-icon class="primary"
-          /></a>
-          <a href="https://www.linkedin.com/in/manthanshh/"
-            ><linked-in-icon class="primary"
-          /></a>
+        >
+          <p id="copyright" class="text-white order-1 md:order-0">
+            All Rights Reserved | Planet Impact Podcast
+            {{ new Date().getFullYear() }}
+          </p>
+          <div class="flex space-x-2 order-0 md:order-1">
+            <a href="https://www.facebook.com/135manthan"
+              ><facebook-icon class="primary"
+            /></a>
+            <a href="https://twitter.com/ManthanShah135"
+              ><twitter-icon class="primary"
+            /></a>
+            <a href="https://www.instagram.com/planetimpactpod/?hl=en"
+              ><instagram-icon class="primary"
+            /></a>
+            <a href="https://www.linkedin.com/in/manthanshh/"
+              ><linked-in-icon class="primary"
+            /></a>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -87,5 +119,9 @@ p {
 
 #copyright {
   @apply text-xs;
+}
+
+#un-support {
+  background-color: rgb(241, 241, 241);
 }
 </style>
