966c61088623e9cf93b2d01931f05cd09f4d805b
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-19 21:44:44 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,23 @@
 <template>
-  <div><p-header heading="About the Host" /></div>
+  <div>
+    <p-header heading="About the Host" />
+    {{ about }}
+    <div class="flex container">
+      <div class="w-1/2">
+        <p>{{ about.description }}</p>
+      </div>
+      <div class="w-1/2"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const [about] = await $content('about').fetch()
+    return { about }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
