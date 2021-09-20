15f5f73b1745d6ecab41b56934e486d1532ae797
FordanG <60175691+FordanG@users.noreply.github.com>
2021-09-21 02:16:18 +0800

    Add Partners, Features, Team, Vote Form to Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <p-testimonials /> -->
     <header>
       <div class="w-full">
         <div class="welcomeHeader">
@@ -10,7 +9,8 @@
               style="height: 240px"
             >
               <h1
-                class="xl:text-5xl lg:text-3xl md:text-2xl text-xl font-bold text-secondary text-center md:text-right"
+                class="xl:text-5xl lg:text-3xl text-2xl font-bold text-secondary text-center md:text-right aos-init aos-animate"
+                data-aos="zoom-in-up"
               >
                 Welcome to the <br />Planet Impact Podcast!
               </h1>
@@ -19,7 +19,7 @@
         </div>
 
         <div class="bg-primary py-4 pb-10">
-          <div class="container">
+          <div class="container px-5 md:px-8 lg:px-12 xl:px-0">
             <div class="flex flex-col lg:flex-row justify-center">
               <div
                 class="w-full lg:w-1/4 flex flex-col items-center text-center md:text-left -mt-12"
@@ -69,7 +69,9 @@
       </div>
     </header>
 
-    <div class="container flex flex-col items-center">
+    <div
+      class="container px-5 md:px-8 lg:px-12 xl:px-0 flex flex-col items-center"
+    >
       <p-header heading="Featured Episodes" />
       <section class="space-y-8 w-full lg:w-2/3">
         <div
@@ -99,27 +101,287 @@
     </div>
 
     <!-- Testimonials -->
-    <section class="container flex flex-col items-center">
-      <div>
-        <p-header heading="Kind Words From Our Listeners" />
-      </div>
+    <section class="bg-primary pb-16">
       <div
-        class="flex lg:flex-row flex-col space-y-5 lg:items-baseline items-center"
+        class="container flex flex-col items-center px-5 md:px-8 lg:px-12 xl:px-0"
       >
-        <p-testimonial
-          v-for="testimonial in landing.testimonials"
-          :key="testimonial.name"
-          :details="testimonial"
-        />
+        <div>
+          <p-header heading="Kind Words From Our Listeners" />
+        </div>
+        <div
+          class="flex lg:flex-row flex-col space-y-5 lg:items-baseline items-center"
+        >
+          <p-testimonial
+            v-for="testimonial in landing.testimonials"
+            class="aos-init aos-animate"
+            data-aos="flip-left"
+            :key="testimonial.name"
+            :details="testimonial"
+          />
+        </div>
+      </div>
+    </section>
+
+    <!-- Partners -->
+    <section id="partners" class="mb-20">
+      <div class="container px-5 md:px-8 lg:px-12 xl:px-0">
+        <div class="flex justify-center">
+          <p-header heading="Our Partners" />
+        </div>
+        <div
+          class="flex lg:flex-row flex-col lg:space-x-12 space-y-16 lg:space-y-0 justify-center items-center "
+        >
+          <div
+            v-for="partner in landing.partners"
+            :key="partner.name"
+            class="w-4/5 lg:w-auto"
+          >
+            <a :href="partner.url" target="_blank">
+              <img
+                class="aos-init aos-animate lg:h-16 h-auto "
+                data-aos="zoom-in-up"
+                :src="partner.logo"
+                :alt="`${partner.name} logo`"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Features -->
+    <section id="features" class="mb-20">
+      <div class="container px-5 md:px-8 lg:px-12 xl:px-0">
+        <div class="flex justify-center">
+          <p-header heading="Featured In" />
+        </div>
+        <div
+          class="flex lg:flex-row flex-col lg:space-x-12 space-y-16 lg:space-y-0 justify-center items-center "
+        >
+          <div
+            v-for="feature in landing.features"
+            :key="feature.name"
+            class="w-4/5 lg:w-auto"
+          >
+            <a :href="feature.url" target="_blank">
+              <img
+                class="aos-init aos-animate lg:h-16 h-auto "
+                data-aos="zoom-in-up"
+                :src="feature.logo"
+                :alt="`${feature.name} logo`"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Team -->
+    <section id="team" class="bg-primary pb-8">
+      <div class="container px-5 md:px-8 lg:px-12 xl:px-0">
+        <div class="flex justify-center mb-4">
+          <p-header heading="Our Team" />
+        </div>
+        <div
+          class="flex md:flex-row flex-col items-center space-y-12 md:space-y-0 justify-center text-center"
+        >
+          <div
+            class="card md:w-1/4 w-full aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            <div class="card-content">
+              <div class="card-body p-0">
+                <div class="profile flex justify-center">
+                  <img
+                    src="~assets/img/Joe.jpeg"
+                    class="border-4 border-secondary shadow"
+                  />
+                </div>
+                <div class="card-title">
+                  <h3 class="font-semibold">Joe Hawthorne</h3>
+                  <small>Producer</small>
+                </div>
+                <div class="card-subtitle">
+                  <p class="mb-0">
+                    <a
+                      href="https://www.linkedin.com/in/joseph-eden-hawthorne-84650089/"
+                      target="_blank"
+                      ><i class="fa fa-linkedin" aria-hidden="true"></i
+                    ></a>
+                    <a
+                      href="https://www.josephedenhawthorne.com/"
+                      target="_blank"
+                      ><i class="fa fa-globe" aria-hidden="true"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="card md:w-1/4 w-full aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            <div class="card-content">
+              <div class="card-body p-0">
+                <div class="profile flex justify-center">
+                  <img
+                    src="~assets/img/Indraneel.png"
+                    class="border-4 border-secondary shadow"
+                  />
+                </div>
+                <div class="card-title">
+                  <h3 class="font-semibold">Indraneel Joshi</h3>
+                  <small>Graphic Designer</small>
+                </div>
+                <div class="card-subtitle">
+                  <p class="mb-0">
+                    <a
+                      href="https://www.instagram.com/indrawneel/?hl=en"
+                      target="_blank"
+                      ><i class="fa fa-instagram" aria-hidden="true"></i
+                    ></a>
+                    <a
+                      href="https://www.linkedin.com/in/indraneel-joshi-a00b3a1b7/"
+                      target="_blank"
+                      ><i class="fa fa-linkedin" aria-hidden="true"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="card md:w-1/4 w-full aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            <div class="card-content">
+              <div class="card-body p-0">
+                <div class="profile flex justify-center">
+                  <img
+                    src="~assets/img/gab-pic.png"
+                    class="border-4 border-secondary shadow"
+                  />
+                </div>
+                <div class="card-title">
+                  <h3 class="font-semibold">Gabriel Fordan</h3>
+                  <small>Web Developer</small>
+                </div>
+                <div class="card-subtitle">
+                  <p class="mb-0">
+                    <a
+                      href="https://www.instagram.com/gabrieldf29"
+                      target="_blank"
+                      ><i class="fa fa-instagram" aria-hidden="true"></i
+                    ></a>
+                    <a
+                      href="https://www.linkedin.com/in/gabrielfordan/"
+                      target="_blank"
+                      ><i class="fa fa-linkedin" aria-hidden="true"></i
+                    ></a>
+                    <a href="https://gabrielfordan.xyz/" target="_blank"
+                      ><i class="fa fa-globe" aria-hidden="true"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="card md:w-1/4 w-full aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            <div class="card-content">
+              <div class="card-body p-0">
+                <div class="profile flex justify-center">
+                  <img
+                    src="~assets/img/host.png"
+                    class="border-4 border-secondary shadow"
+                  />
+                </div>
+                <div class="card-title">
+                  <h3 class="font-semibold">Manthan Shah</h3>
+                  <small>Founder and Host</small>
+                </div>
+                <div class="card-subtitle">
+                  <p class="mb-0">
+                    <a
+                      href="https://www.instagram.com/manthanshh"
+                      target="_blank"
+                      ><i class="fa fa-instagram" aria-hidden="true"></i
+                    ></a>
+
+                    <a
+                      href="https://www.linkedin.com/in/manthanshh/"
+                      target="_blank"
+                      ><i class="fa fa-linkedin" aria-hidden="true"></i
+                    ></a>
+                    <a
+                      href="https://www.facebook.com/135manthan"
+                      target="_blank"
+                      ><i class="fa fa-facebook" aria-hidden="true"></i
+                    ></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="vote-form">
+      <div class="container px-5 md:px-8 lg:px-12 xl:px-0">
+        <div class="flex justify-center">
+          <div class="col">
+            <div class="well well-sm">
+              <form
+                class="form-horizontal"
+                name="Voting Form"
+                method="post"
+                data-netlify="true"
+              >
+                <fieldset>
+                  <p-header
+                    heading="Tell us who you want to be our next guest!"
+                  />
+
+                  <!-- Name input-->
+                  <div class="form-group flex justify-center mb-2">
+                    <div class="w-full lg:w-1/2 text-center">
+                      <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Name of Guest"
+                        class="text-center appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                      />
+                    </div>
+                  </div>
+
+                  <!-- Form actions -->
+                  <div class="form-group">
+                    <div class="col text-center">
+                      <button
+                        type="submit"
+                        class="shadow w-full lg:w-1/2 border-2 border-primary hover:bg-primary focus:shadow-outline focus:outline-none text-primary hover:text-white font-bold py-2 px-4 rounded"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import PTestimonials from '../components/PTestimonials.vue'
 export default {
-  components: { PTestimonials },
   async asyncData({ $content }) {
     const landing = await $content('landing').fetch()
 
@@ -142,29 +404,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import 'aos/dist/aos.css';
 .welcomeHeader {
   background-image: url(~assets/img/header.svg);
   background-size: cover;
   background-repeat: no-repeat;
-  /* backdrop-filter: opacity(50%);
-  backdrop-filter: brightness(150%);
-  backdrop-filter: contrast(40%); */
-  /* height: 240px; */
 }
 
-.host {
-  margin-top: -45px;
+#vote-form {
+  padding: 2rem 0;
 }
 
-.host h2 {
-  font-size: 28px;
-  font-style: normal;
+#vote-form h3 {
   color: #22486a;
 }
 
-.host h3 {
-  font-size: 20px;
-  font-style: normal;
+#team h2 {
   color: #fff;
+}
+
+#team .icon {
+  position: relative;
+  bottom: 11px;
+}
+
+#team .profile img {
+  width: 85px;
+  height: 85px;
+  border-radius: 50%;
+}
+
+#team .col-md-3 {
+  padding: 0 1rem;
+}
+
+#team .card {
+  padding: 0.5rem 2rem;
+  border-radius: 15px;
+  background-color: transparent;
+  border: none;
+}
+
+#team .card-content {
+  background-color: white;
+  padding: 0rem 2rem;
+  border-radius: 15px;
+}
+
+#team .card-body {
+  position: relative;
+  bottom: 35px;
+}
+
+#team .header {
+  padding-top: 10px;
+}
+
+#team a {
+  color: #94c11f;
+  padding: 0 10px;
+}
+
+#team a:hover {
+  color: #4b6113;
 }
 </style>
