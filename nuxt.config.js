4b777670f4551e9a7bb2bf026e993f0f28a7481b
FordanG <60175691+FordanG@users.noreply.github.com>
2021-10-14 23:59:55 +0800

    Fix Embedded Image File Link

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
         content:
           'Planet Impact Podcast is a weekly show on how non-conformist social entrepreneurs are changing the world.'
       },
-      { property: 'og:image', content: '/embed-image.png' },
+      { property: 'og:image', content: '/img/embed-image.png' },
 
       // <!-- Open Graph / Twitter -->
       { property: 'twitter:card', content: 'summary_large_image' },
@@ -40,7 +40,7 @@ export default {
         content:
           'Planet Impact Podcast is a weekly show on how non-conformist social entrepreneurs are changing the world.'
       },
-      { property: 'twitter:image', content: '/embed-image.png' },
+      { property: 'twitter:image', content: '/img/embed-image.png' },
 
       // Google Site Verification
       {
